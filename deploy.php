c20c2332a3f2daa057aee3137294ccfb5df45254
Jarieliz-Capstone <kumar@lucidinfosystems.com>
2018-07-21 15:23:19 +0000

    first commit

---
+++ deploy.php
@@ -0,0 +1,41 @@
+<?php
+namespace Deployer;
+
+require 'recipe/yii2-app-advanced.php';
+
+// Project name
+set('application', 'basic');
+
+// Project repository
+set('repository', '');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true); 
+
+// Shared files/dirs between deploys 
+add('shared_files', []);
+add('shared_dirs', []);
+
+// Writable dirs by web server 
+add('writable_dirs', []);
+
+
+// Hosts
+
+host('132.148.158.23')
+    ->set('deploy_path', '~/var/www/html/apps/basic');    
+    
+// Tasks
+task('test', function () {
+    writeln('Hello');
+});
+
+
+task('build', function () {
+    run('cd {{release_path}} && build');
+});
+
+// [Optional] if deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
+
+
